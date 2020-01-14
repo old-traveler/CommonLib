85ef8b8911affadb611728c98a379e383aee71b0
贺宇成 <heyucheng001@ke.com>
2020-01-14 16:50:54 +0800

    [Change]: [NetCachePool]

--- netcache/src/main/java/com/hyc/netcache/SpNetCachePool.kt
+++ netcache/src/main/java/com/hyc/netcache/SpNetCachePool.kt
@@ -2,6 +2,10 @@ package com.hyc.netcache
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.preference.PreferenceManager
 import android.util.LruCache
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
@@ -14,10 +18,38 @@ import com.google.gson.reflect.TypeToken
  */
 class SpNetCachePool(context: Context) : NetCachePool {
 
-  private val mLruCache: LruCache<String, NetCacheBean<*>> = LruCache(50)
-  private val mSpCache: SharedPreferences =
-    context.getSharedPreferences("BkNetCache", Context.MODE_PRIVATE)
+  private val spVersionCodeKey = "BkNetCacheVersionCode"
+  private val spNetCachePrefix = "BkNetCache_"
+  private val mLruCache: LruCache<String, NetCacheBean<*>> = LruCache(100)
+  private val mSpCache: SharedPreferences
   private val mGosn: Gson = Gson()
+
+  init {
+    val versionSp = PreferenceManager.getDefaultSharedPreferences(context.applicationContext)
+    val oldCode = versionSp.getLong(spVersionCodeKey, 0)
+    var appVersionCode = 0L
+    try {
+      val packageInfo = context.applicationContext
+        .packageManager
+        .getPackageInfo(context.packageName, 0)
+      appVersionCode = if (VERSION.SDK_INT >= VERSION_CODES.P) {
+        packageInfo.longVersionCode
+      } else {
+        @Suppress("DEPRECATION")
+        packageInfo.versionCode.toLong()
+      }
+    } catch (e: PackageManager.NameNotFoundException) {
+      e.printStackTrace()
+    }
+    if (appVersionCode > oldCode) {
+      //app进行了升级，需要删除原来的网络缓存并更新网络缓存版本号
+      context.getSharedPreferences("$spNetCachePrefix$oldCode", Context.MODE_PRIVATE).edit().clear()
+        .apply()
+      versionSp.edit().putLong(spVersionCodeKey, appVersionCode).apply()
+    }
+    val newCode = appVersionCode.coerceAtLeast(oldCode)
+    mSpCache = context.getSharedPreferences("$spNetCachePrefix$newCode", Context.MODE_PRIVATE)
+  }
 
   override fun addNetCache(key: String, any: Any) {
     addNetCache(key, any, getDefaultConfig())
@@ -48,7 +80,7 @@ class SpNetCachePool(context: Context) : NetCachePool {
     try {
       val type = object : TypeToken<NetCacheBean<T>>() {}.type
       val data: NetCacheBean<T>? = mGosn.fromJson(json, type)
-      return getRealData(data)?.data as? T
+      return getRealData(data, false)?.data as? T
     } catch (e: JsonSyntaxException) {
       e.printStackTrace()
     }
@@ -65,12 +97,16 @@ class SpNetCachePool(context: Context) : NetCachePool {
     mSpCache.edit().clear().apply()
   }
 
-  private fun getRealData(cacheBean: NetCacheBean<*>?): NetCacheBean<*>? {
+  private fun getRealData(cacheBean: NetCacheBean<*>?, isSaved: Boolean = true): NetCacheBean<*>? {
     cacheBean ?: return null
     if (isTimeOut(cacheBean)) {
       return null
     }
-    return mLruCache.put(cacheBean.url, cacheBean)
+    if (!isSaved) {
+      mLruCache.put(cacheBean.url, cacheBean)
+    }
+    //返回未超时的网络缓存数据
+    return cacheBean
   }
 
   private fun isTimeOut(cacheBean: NetCacheBean<*>): Boolean {
